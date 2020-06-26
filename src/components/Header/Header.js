385dd50c004ec3103810961ccc9c7b94a3c90f62
dtrmangesh <mangesh.datar55@gmail.com>
2020-06-26 16:50:46 +0530

    Fixed excelsheet issue.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -27,7 +27,7 @@ export default class Header extends Component {
                     <Grid item xs={3}>
 
                         <span className='inputcontainer'>
-                            <input className="input" placeholder="Search Employee" id="search" autocomplete="off" onChange={e => this.getSearchQuery(e)}/>
+                            <input className="input" placeholder="Search Employee" id="search"  autocomplete="off" onChange={e => this.getSearchQuery(e)}/>
                         </span>
                     </Grid>
                     <Grid item xs={3}>
